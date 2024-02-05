0425fd0b49f29694e9717c92fcf6f524676902a0
Nancy Hunter <hunternancym@gmail.com>
2024-02-05 17:43:32 +0700

    first push

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const app = express()
+const PORT = 8000
+
+const ChiangMaiRestaurants = {
+    'king of noodles' : {
+        address: "",
+        favoriteDish: 'Crispy Pork, noodles, and wonton dumplings',
+        hours: '2PM-10PM',
+        price: '60 baht',
+    }, 
+    'Good Smells' : {
+        address: "",
+        favoriteDish: 'Pad Kee Mow',
+        hours: '7AM -2PM',
+        price: '50-60 baht',
+    },'Good Smells 2' : {
+        address: "",
+        favoriteDish: 'Crispy Pork and morning glory over rice with an egg',
+        hours: '10AM -5PM',
+        price: '50-60 baht',
+    },'Je Hoa' : {
+        address: "",
+        favoriteDish: 'chicken or shrimp dumplings with a bowl of noodles and sliced pork AND mantou desser',
+        hours: '11AM -7PM',
+        price: '90-120 baht',
+    },
+}
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+})
+app.get('/API', (req, res) => {
+    res.json(ChiangMaiRestaurants)
+})
+app.listen(PORT, () => {
+    console.log(`The server is running on ${PORT}`)
+})
